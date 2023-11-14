cd05fb05b0702fb5c0953e104efa5a8d5f2609cb
vill4in <j.adrian.pulido.s@gmail.com>
2023-11-14 01:50:09 -0600

    GUI almost done

---
+++ main.py
@@ -0,0 +1,12 @@
+from maingui import *
+
+factor = 2.5
+maingui = MainGui(title='Typing Speed Test',
+                  geometry=[int(900*factor), int(600*factor)])
+
+if __name__ == "__main__":
+    maingui.mainloop()
+
+
+
+
